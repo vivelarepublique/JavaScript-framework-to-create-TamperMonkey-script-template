719b1b4b9db7606aa97e006a67cc67b4e075c76a
Rex <vivelarepublique@qq.com>
2024-05-23 01:19:27 +0800

    css template update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Modal from './components/Modal.vue';
-import { storeToRefs } from 'pinia';
-import { useSwitcherStore } from './store/switcher';
+    import Modal from './components/Modal.vue';
+    import { storeToRefs } from 'pinia';
+    import { useSwitcherStore } from './store/switcher';
 
-const store = useSwitcherStore();
-const { show } = storeToRefs(store);
-const { open } = store;
+    const store = useSwitcherStore();
+    const { show } = storeToRefs(store);
+    const { open } = store;
 </script>
 
 <template>
@@ -14,20 +14,20 @@ const { open } = store;
 </template>
 
 <style>
-#show-modal {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    animation: animetop 0.25s;
-    padding: 10px 20px;
-    margin: 5px 10px;
-    border: none;
-    background-color: #007bff;
-    color: white;
-    font-weight: bold;
-    border-radius: 5px;
-    cursor: pointer;
-}
+    #show-modal {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        animation: animetop 0.25s;
+        padding: 10px 20px;
+        margin: 5px 10px;
+        border: none;
+        background-color: #007bff;
+        color: white;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,55 +2,58 @@
     <div class="modal-mask" @click.stop.self="close">
         <div class="modal-container">
             <span><button class="modal-close-button" @click="close">&times;</button></span>
-            <div id="main-app" class="flex-div">
-                <HelloWorld msg="Welcome" />
-                <Counter />
-                <Bridge />
+            <div class="b4-container">
+                <div class="b4-row">
+                    <HelloWorld msg="Welcome" class="b4-col" />
+                    <Counter class="b4-col" />
+                    <Bridge class="b4-col" />
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from './HelloWorld.vue';
-import Counter from './Counter.vue';
-import Bridge from './Bridge.vue';
+    import HelloWorld from './HelloWorld.vue';
+    import Counter from './Counter.vue';
+    import Bridge from './Bridge.vue';
 
-import { useSwitcherStore } from '../store/switcher';
-const switcher = useSwitcherStore();
-const { close } = switcher;
+    import { useSwitcherStore } from '../store/switcher';
+    const switcher = useSwitcherStore();
+    const { close } = switcher;
 </script>
 
 <style>
-.modal-mask {
-    position: fixed;
-    z-index: 9999;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    padding-top: 20px;
-    overflow: auto;
-    min-width: 1200px;
-    max-height: 95vh;
-    animation: animetop 0.25s;
-}
+    .modal-mask {
+        position: fixed;
+        z-index: 9999;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        padding-top: 20px;
+        overflow: auto;
+        min-width: 1200px;
+        max-height: 95vh;
+        animation: animetop 0.25s;
+        font-size: 2rem;
+    }
 
-.modal-container {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 95%;
-    min-height: 80vh;
-}
+    .modal-container {
+        background-color: #fefefe;
+        margin: auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 95%;
+        min-height: 80vh;
+    }
 
-.modal-close-button {
-    color: #aaa;
-    float: right;
-    font-size: 24px;
-    font-weight: bold;
-}
+    .modal-close-button {
+        color: #aaa;
+        float: right;
+        font-size: 24px;
+        font-weight: bold;
+    }
 </style>
