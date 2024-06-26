da592abb873bb66db09dc754ba5e2a1ba02e8214
Rex <vivelarepublique@qq.com>
2024-06-26 11:18:34 +0800

    minify plugin code

--- plugin/vite-plugin-tampermonkey-template.js
+++ plugin/vite-plugin-tampermonkey-template.js
@@ -1,12 +1,12 @@
 import { build } from 'vite';
 
 const cssInjectedByJsId = '\0vite/all-css';
-async function buildCSSInjectionCode({ buildOptions, cssToInject }) {
+async function buildCSSInjectionCode({ buildOptions, css }) {
     const { minify, target } = buildOptions;
     const res = await build({
         root: '',
         configFile: false,
-        plugins: [injectionCSSCodePlugin({ cssToInject })],
+        plugins: [injectionCSSCodePlugin({ css })],
         build: {
             write: false,
             target,
@@ -31,106 +31,44 @@ async function buildCSSInjectionCode({ buildOptions, cssToInject }) {
     return _cssChunk.output[0];
 }
 
-function resolveInjectionCode(cssCode) {
-    const injectFunction = cssCode => /*javascript*/ `const otherCss = document.createElement('style');
-otherCss.appendChild(document.createTextNode(${cssCode}));
-document.head.appendChild(otherCss);`;
-    return injectFunction(cssCode);
-}
-
-function injectionCSSCodePlugin({ cssToInject }) {
+function injectionCSSCodePlugin({ css }) {
     return {
         name: 'vite:injection-css-code-plugin',
         resolveId(id) {
-            if (id == cssInjectedByJsId) {
+            if (id === cssInjectedByJsId) {
                 return id;
             }
         },
         load(id) {
-            if (id == cssInjectedByJsId) {
-                const cssCode = JSON.stringify(cssToInject.trim());
-                return resolveInjectionCode(cssCode);
+            if (id === cssInjectedByJsId) {
+                const cssCode = JSON.stringify(css.trim());
+                const injectFunction = cssCode => /*javascript*/ `const otherCss = document.createElement('style');
+                    otherCss.appendChild(document.createTextNode(${cssCode}));
+                    document.head.appendChild(otherCss);`;
+                return injectFunction(cssCode);
             }
         },
     };
 }
 
-function isJsOutputChunk(chunk) {
-    return chunk.type == 'chunk' && chunk.fileName.match(/.[cm]?js(?:\?.+)?$/) != null;
-}
-function defaultJsAssetsFilter(chunk) {
-    return chunk.isEntry && !chunk.fileName.includes('polyfill');
-}
-
-const cssSourceCache = {};
-function extractCss(bundle, cssName) {
-    const cssAsset = bundle[cssName];
-    if (cssAsset !== undefined && cssAsset.source) {
-        const cssSource = cssAsset.source;
-        cssSourceCache[cssName] = cssSource instanceof Uint8Array ? new TextDecoder().decode(cssSource) : `${cssSource}`;
-    }
-    return cssSourceCache[cssName] ?? '';
-}
-function concatCssAndDeleteFromBundle(bundle, cssAssets) {
-    return cssAssets.reduce((previous, cssName) => {
-        const cssSource = extractCss(bundle, cssName);
-        delete bundle[cssName];
-        return previous + cssSource;
-    }, '');
-}
-
-function getJsTargetBundleKeys(bundle) {
-    const jsAssets = Object.keys(bundle).filter(i => {
-        const asset = bundle[i];
-        return isJsOutputChunk(asset) && defaultJsAssetsFilter(asset);
-    });
-    if (jsAssets.length == 0) {
-        return [];
-    }
-    const jsTargetFileName = jsAssets[jsAssets.length - 1];
-    return [jsTargetFileName];
-}
-
-const globalCSSCodeEntryCache = new Map();
-let previousFacadeModuleId = '';
 async function globalCssInjection(bundle, cssAssets, buildCssCode, banner) {
-    const jsTargetBundleKeys = getJsTargetBundleKeys(bundle);
-    if (jsTargetBundleKeys.length === 0) return;
+    const jsTargetBundleKeys = Object.keys(bundle).filter(e => bundle[e].type == 'chunk' && bundle[e].isEntry && bundle[e].fileName.match(/.[cm]?js(?:\?.+)?$/) != null);
 
-    const allCssCode = concatCssAndDeleteFromBundle(bundle, cssAssets);
-    let cssInjectionCode = '';
-    if (allCssCode.length > 0) {
-        const cssCode = (await buildCssCode(allCssCode))?.code;
-        if (typeof cssCode == 'string') {
-            cssInjectionCode = cssCode;
-        }
-    }
-    for (const jsTargetKey of jsTargetBundleKeys) {
-        const jsAsset = bundle[jsTargetKey];
+    if (jsTargetBundleKeys.length === 0) throw new Error('No entry point found');
 
-        if (jsAsset.facadeModuleId != null && jsAsset.isEntry && cssInjectionCode != '') {
-            if (jsAsset.facadeModuleId != previousFacadeModuleId) {
-                globalCSSCodeEntryCache.clear();
-            }
-            previousFacadeModuleId = jsAsset.facadeModuleId;
-            globalCSSCodeEntryCache.set(jsAsset.facadeModuleId, cssInjectionCode);
-        }
-        if (cssInjectionCode == '' && jsAsset.isEntry && jsAsset.facadeModuleId != null && typeof globalCSSCodeEntryCache.get(jsAsset.facadeModuleId) == 'string') {
-            cssInjectionCode = globalCSSCodeEntryCache.get(jsAsset.facadeModuleId);
-        }
+    const allCssCode = cssAssets.reduce((accumulator, currentValue) => {
+        const cssSource = bundle[currentValue].source instanceof Uint8Array ? new TextDecoder().decode(bundle[currentValue].source) : `${bundle[currentValue].source}`;
+        delete bundle[currentValue];
+        return accumulator + cssSource;
+    }, '');
 
-        jsAsset.code = buildOutputChunkWithCssInjectionCode(jsAsset.code, cssInjectionCode ?? '', banner);
-    }
-}
+    if (allCssCode.length === 0) return null;
 
-function buildOutputChunkWithCssInjectionCode(jsAssetCode, cssInjectionCode, banner) {
-    const appCode = jsAssetCode.replace(/\/\*\s*empty css\s*\*\//g, '');
-    return /*javascript*/ `${banner}
-(function () {
-    'use strict';
-    ${cssInjectionCode}
-    ${appCode}
-})();`;
+    const cssCode = (await buildCssCode(allCssCode))?.code;
+
+    jsTargetBundleKeys.forEach(js => {
+        bundle[js].code = /*javascript*/ `${banner}${'\n'}(function () {${'\n'}'use strict';${'\n'}${cssCode}${'\n'}${bundle[js].code}${'\n'}})();`;
+    });
 }
 
 export default function plugin({ banner }) {
@@ -142,26 +80,16 @@ export default function plugin({ banner }) {
             apply: 'build',
             enforce: 'post',
             name: 'vite-plugin-tampermonkey-template',
-            config(config, { command }) {
-                if (command === 'build') {
-                    if (!config.build) {
-                        config.build = {};
-                    }
-                }
-            },
             configResolved(resolvedConfig) {
                 viteConfig.config = resolvedConfig;
             },
             async generateBundle(_, bundle) {
-                if (viteConfig.config.build.ssr) {
-                    return;
-                }
-                const buildCssCode = cssToInject =>
+                const buildCssCode = css =>
                     buildCSSInjectionCode({
                         buildOptions: viteConfig.config.build,
-                        cssToInject,
+                        css,
                     });
-                const cssAssets = Object.keys(bundle).filter(i => bundle[i].type == 'asset' && bundle[i].fileName.endsWith('.css'));
+                const cssAssets = Object.keys(bundle).filter(e => bundle[e].type === 'asset' && bundle[e].fileName.endsWith('.css'));
 
                 await globalCssInjection(bundle, cssAssets, buildCssCode, banner);
             },
