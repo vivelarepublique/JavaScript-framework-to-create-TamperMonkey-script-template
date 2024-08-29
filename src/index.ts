438a7dc915ff5b4b132e83e0f2df4659bab82fab
Rex <vivelarepublique@qq.com>
2024-08-29 15:46:00 +0800

    tsconfig error bug fix

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,11 @@ import './styles/test.less';
 import './styles/test.sass';
 import './styles/test.styl';
 
-//@ts-ignore
 import { createVueApp } from './vue';
-//@ts-ignore
 import { createReactApp } from './react';
-//@ts-ignore
 import { createPreactApp } from './preact';
-//@ts-ignore
 import { createLitApp } from './lit';
-//@ts-ignore
 import SvelteApp from './svelte';
-//@ts-ignore
 import { createSolidApp } from './solid';
 
 import { someTestActions } from './action/beforeMountActions';
