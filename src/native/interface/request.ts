7bfd8108d2b8dad951c8b40042e5a27c5d2de25b
Rex <vivelarepublique@qq.com>
2024-09-12 11:29:14 +0800

    request type update

--- src/native/interface/request.ts
+++ src/native/interface/request.ts
@@ -1,8 +1,24 @@
 export type RequestMethods = 'GET' | 'HEAD' | 'POST';
 
+export interface RequestHeaders {
+    readonly [header: string]: string;
+}
+
 export interface TampermonkeyWebRequestParameters {
     url: string;
     method: RequestMethods;
-    data?: any;
-    headers?: any;
+    headers?: RequestHeaders;
+    data?: string;
+    redirect?: 'follow' | 'error' | 'manual';
+    cookie?: string;
+    binary?: boolean;
+    nocache?: boolean;
+    revalidate?: boolean;
+    timeout?: number;
+    responseType?: 'arraybuffer' | 'blob' | 'json' | 'stream';
+    overrideMimeType?: string;
+    anonymous?: boolean;
+    fetch?: boolean;
+    user?: string;
+    password?: string;
 }
