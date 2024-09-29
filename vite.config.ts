476935545f22f5075c474923c478a559abf0a5e9
Rex <vivelarepublique@qq.com>
2024-09-29 23:35:53 +0800

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, type PluginOption } from 'vite';
+import { defineConfig } from 'vite';
+import type { PluginOption } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
 import react from '@vitejs/plugin-react';
@@ -16,47 +17,61 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 import { bannerConfig } from './config/getParameters';
 
-export default defineConfig({
-    build: {
-        target: 'esnext',
-        outDir: 'release',
-        assetsInlineLimit: 4096 * 1024,
-        chunkSizeWarningLimit: 2048 * 1024,
-        minify: 'terser',
-        terserOptions: {
-            compress: true,
-            mangle: false,
-            format: {
-                beautify: true,
+export default defineConfig(({ command }) => {
+    const isBuild = command === 'build';
+
+    const buildPlugins: PluginOption[] = isBuild
+        ? [
+              cssBeautificationAndExternalCssTreeShaking({ cssPath: 'node_modules/bootstrap/dist/css/bootstrap.min.css', framework: ['vue', 'react', 'preact', 'lit', 'svelte', 'solid'] }),
+              tampermonkeyBannerAdditionAndCssInjection({ bannerConfig }),
+              visualizer({
+                  emitFile: true,
+                  filename: 'stats.html',
+              }) as PluginOption,
+          ]
+        : [];
+
+    return {
+        server: {
+            port: 5267,
+            cors: true,
+        },
+        build: {
+            target: 'esnext',
+            outDir: 'release',
+            assetsInlineLimit: 4096 * 1024,
+            chunkSizeWarningLimit: 2048 * 1024,
+            minify: 'terser',
+            terserOptions: {
+                compress: true,
+                mangle: false,
+                format: {
+                    beautify: true,
+                },
+            },
+            rollupOptions: {
+                input: './src/index.ts',
+                output: {
+                    entryFileNames: `${bannerConfig.name}.user.js`,
+                },
             },
         },
-        rollupOptions: {
-            input: './src/index.ts',
-            output: {
-                entryFileNames: `${bannerConfig.name}.user.js`,
-            },
-        },
-    },
-    plugins: [
-        vue(),
-        react({
-            include: ['src/react/**/*.{tsx,ts,jsx,js}'],
-        }),
-        preact({
-            include: ['src/preact/**/*.{tsx,ts,jsx,js}'],
-        }),
-        svelte({
-            include: ['src/svelte/**/*.{tsx,ts,jsx,js,svelte}'],
-            preprocess: vitePreprocess(),
-        }),
-        solid({
-            include: ['src/solid/**/*.{tsx,ts,jsx,js}'],
-        }),
-        cssBeautificationAndExternalCssTreeShaking({ cssPath: 'node_modules/bootstrap/dist/css/bootstrap.min.css', framework: ['vue', 'react', 'preact', 'lit', 'svelte', 'solid'] }),
-        tampermonkeyBannerAdditionAndCssInjection({ bannerConfig }),
-        visualizer({
-            emitFile: true,
-            filename: 'stats.html',
-        }) as PluginOption,
-    ],
+        plugins: [
+            vue(),
+            react({
+                include: ['src/react/**/*.{tsx,ts,jsx,js}'],
+            }),
+            preact({
+                include: ['src/preact/**/*.{tsx,ts,jsx,js}'],
+            }),
+            svelte({
+                include: ['src/svelte/**/*.{tsx,ts,jsx,js,svelte}'],
+                preprocess: vitePreprocess(),
+            }),
+            solid({
+                include: ['src/solid/**/*.{tsx,ts,jsx,js}'],
+            }),
+            ...buildPlugins,
+        ],
+    };
 });
